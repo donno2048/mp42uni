5473d0566728a40882b626296998ce1ea2f9d4c6
Elisha Hollander <just4now666666@gmail.com>
2021-04-12 04:57:08 +0300

    Update __init__.py

--- mp42uni/__init__.py
+++ mp42uni/__init__.py
@@ -2,12 +2,12 @@ from curses import wrapper
 from os import system
 from sys import argv
 def play(window):
-	for l in open(__file__.replace("py", "txt")):
+	for l in open(__file__.replace(".py", ".txt")):
 		# time.sleep(x) # Use this to change the fps
 		if l[0] == 'R': window.move(0,0)
 		else:
 			window.addstr(l)
 			window.refresh()
 def main():
-	system(f"python3 {__file__.replace('__init__', 'process')} {argv[1]} > {__file__.replace('py', 'txt')}")
+	system(f"python3 {__file__.replace('__init__', 'process')} {argv[1]} > {__file__.replace('.py', '.txt')}")
 	wrapper(play)
