d36ad0dcfe98cbf0b3285a537d5dd1586fa61c4f
donno2048 <just4now666666@gmail.com>
2021-04-12 15:34:55 +0300

    Don't need preprocess

--- mp42uni/__init__.py
+++ mp42uni/__init__.py
@@ -1,13 +1,33 @@
 from curses import wrapper
 from os import system
 from sys import argv
+try: from PIL.Image import open
+except: from Image import open
+from cv2 import VideoCapture, imwrite, resize
 def play(window):
-	for l in open(__file__.replace(".py", ".txt")):
-		# time.sleep(x) # Use this to change the fps
-		if l[0] == 'R': window.move(0,0)
-		else:
-			window.addstr(l)
+	vidcap = VideoCapture(argv[1])
+	success, image = vidcap.read()
+	while success:
+		imwrite(__file__.replace(".py", ".jpg"), resize(image, (144, 108), interpolation = 3))
+		for l in I2T(__file__.replace(".py", ".jpg")).split("\n"):
+			# time.sleep(x) # Use this to change the fps
+			window.addstr(l + "\n")
 			window.refresh()
-def main():
-	system(f"python3 {__file__.replace('__init__', 'process')} {argv[1]} > {__file__.replace('.py', '.txt')}")
-	wrapper(play)
+		window.move(0, 0)
+		success, image = vidcap.read()
+def I2T(File):
+	text = ""
+	im = open(File)
+	counter = 0
+	field = True
+	for pixel in list(im.convert("1").getdata()):
+		if field:
+			if pixel > 127: text += "*"
+			else: text += " "
+		counter += 1
+		if counter >= im.size[0]:
+			counter = 0
+			if field: text += "\n"
+			field = not field
+	return text
+def main(): wrapper(play)
